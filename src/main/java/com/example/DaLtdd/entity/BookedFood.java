0d517b7509e0e9aa73c8b85a175a74cdd8e865ae
sonhong9824 <nghongson9824@gmail.com>
2025-04-10 20:31:44 +0700

    update Ticket

--- src/main/java/com/example/DaLtdd/entity/BookedFood.java
+++ src/main/java/com/example/DaLtdd/entity/BookedFood.java
@@ -7,21 +7,15 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-
-import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Food {
+public class BookedFood {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
-    private String name;
-    private String description;
-    private int price;
-    private String pictureUrl;
+    private int quantity;
+
 }
